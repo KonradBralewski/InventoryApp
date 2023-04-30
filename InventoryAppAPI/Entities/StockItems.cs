16f13c8ab093d992ad012928755343a5391062ce
ShleeRE <konradbralewski1111@gmail.com>
2023-04-30 11:14:38 +0200

    Change guid to int in StockItems class

--- InventoryAppAPI/Entities/StockItems.cs
+++ InventoryAppAPI/Entities/StockItems.cs
@@ -6,7 +6,7 @@ namespace InventoryAppAPI.Entities
     {
         public int Id { get; set; }
         public string Code { get; set; }
-        public Guid ProductId { get; set; }
+        public int ProductId { get; set; }
         public Product Product { get; set; }
         public Guid LocationId { get; set; }
         public Location Location { get; set; }
